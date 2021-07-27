5818eda6f652c754e558470cfff2f3725ac3af6c
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-07-26 20:21:09 -0600

    initialized api

--- django_rest_framework_tutorial/django_rest_framework_tutorial/views.py
+++ django_rest_framework_tutorial/django_rest_framework_tutorial/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework.response import Response
 
+class TestView(APIView):
+    def get(self, request, *args, **kwargs):
+        data = {
+            "username": "admin",
+            "years": 10
+        }
+        return Response(data)
+    
+    
