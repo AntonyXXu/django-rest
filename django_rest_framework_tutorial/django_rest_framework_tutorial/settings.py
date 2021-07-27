ba29d3cf7957885b06010166c74dead518cbdf82
AntonyXXu <77988513+AntonyXXu@users.noreply.github.com>
2021-07-26 21:06:30 -0600

    build rest api and token auth

--- django_rest_framework_tutorial/django_rest_framework_tutorial/settings.py
+++ django_rest_framework_tutorial/django_rest_framework_tutorial/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'drfapp'
+    'drfapp',
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTIFICATION_CLASSES': ['rest_framework.authentication.TokenAuthentication']
+}
